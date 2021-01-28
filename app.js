7bd3c33f2d837cad48ac3a39b20d2e97cf0e5c5e
cheikh885 <dcheikhballa9@gmail.com>
2021-01-28 02:09:47 +0100

    image sur mongod avect upload

--- app.js
+++ app.js
@@ -16,58 +16,58 @@ app.set("view engine", "handlebars");
 app.use(express.static("public/images"));
 
 var Storage = multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, "./public/images");
-  },
-  filename: function (req, file, callback) {
-    callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
-  },
+    destination: function(req, file, callback) {
+        callback(null, "./public/images");
+    },
+    filename: function(req, file, callback) {
+        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    },
 });
 
 var upload = multer({
-  storage: Storage,
+    storage: Storage,
 }).single("image"); //Field name and max count
 
 app.get("/", (req, res) => {
 
-    imageData.exec(function(err,data){
-        if(err) throw err;
+    imageData.exec(function(err, data) {
+        if (err) throw err;
 
         console.log(data)
 
-        res.render('home',{records:data})
+        res.render('home', { records: data })
     })
 });
 
 app.post("/", (req, res) => {
-  upload(req, res, function (err) {
-    if (err) {
-      console.log(err);
-      return res.end("Something went wrong");
-    } else {
-      console.log(req.file.path);
-      var imageName = req.file.filename;
+    upload(req, res, function(err) {
+        if (err) {
+            console.log(err);
+            return res.end("Something went wrong");
+        } else {
+            console.log(req.file.path);
+            var imageName = req.file.filename;
 
-      var imageDetails = new imageModel({
-        imagename: imageName,
-      });
+            var imageDetails = new imageModel({
+                imagename: imageName,
+            });
 
-      imageDetails.save(function (err, doc) {
-        if (err) throw err;
+            imageDetails.save(function(err, doc) {
+                if (err) throw err;
 
-        console.log("Image Saved");
+                console.log("Image Saved");
 
-        imageData.exec(function(err,data){
-            if(err) throw err;
+                imageData.exec(function(err, data) {
+                    if (err) throw err;
 
 
-            res.render('home',{records:data,success:true})
-        })
-      });
-    }
-  });
+                    res.render('home', { records: data, success: true })
+                })
+            });
+        }
+    });
 });
 
 app.listen(5000, () => {
-  console.log("App is listening on Port 5000");
+    console.log("App is listening on Port 5000");
 });
